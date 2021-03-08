b5b75141466acad62cfdc3be04b55c7039526ad8
Joona Oja <n0ojjo00@students.oamk.fi>
2021-03-08 17:35:15 +0200

    viimeistelty ja siistitty = Valmis

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import {useState, useEffect} from "react";
 import './App.css';
-
+// poisto napin/linkin kuvake fontawesomesta
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const deletebutton = <FontAwesomeIcon icon={faTrashAlt} />
-const URL = "http://localhost/shoppinglist/";
 
+const URL = "http://localhost/shoppinglist/";
 
 function App() {
   const [item, setItem] = useState(""); // variables for adding items
   const [amount, setAmount] = useState("")
   const [items, setItems] = useState([]); // variable for rendering shopping list
 
-
+//lisäys listalle
   function add(e) {
     e.preventDefault();
     let status = 0;
@@ -46,7 +46,7 @@ function App() {
       }
     )
   }
-
+// poisto listalta
   function remove(id) {
     let status = 0;
     fetch( URL + "/remove.php" ,{
@@ -91,15 +91,19 @@ function App() {
 
   return (
     <div className="App">
-      <h2>Shopping List</h2>
-      <div className="UI" >
+      <h2 className="otsikko" >SHOPPING LIST</h2>
+      
         <form onSubmit={add}>
-          <label>New Item:</label>&nbsp;
-          <input id="input_item" placeholder="Write item description" value={item} onChange={e => setItem(e.target.value)} />&nbsp;
-          <input id="input_amount" placeholder="Amount" value={amount} onChange={e => setAmount(e.target.value)} />&nbsp;
-          <button id="add">Add to List</button>
+          <div className="form">
+            <label>New Item:</label>&nbsp;
+            <input id="input_item" placeholder="Write item description" 
+              value={item} onChange={e => setItem(e.target.value)} />&nbsp;
+            <input id="input_amount" placeholder="Amount" 
+              value={amount} onChange={e => setAmount(e.target.value)} />&nbsp;
+            <button id="add">Add to List</button>
+          </div>
         </form>
-      </div>
+      
       <div className="list" >
         <table style={{width: "100%"}} >
           <colgroup>
@@ -115,9 +119,12 @@ function App() {
             </tr>
             {items.map((item, i) => ( // yritin saada conditional styling niin, että alimman rivin reunat pyöristyisi, en saanut kuitenkaan toimimaan uusia rivejä lisätessä
             <tr key={item.id} >
-              <td style={items.length -1 === i ? {borderBottomLeftRadius: "15px"} : {} } className="itemdesc" >&nbsp;&nbsp;&nbsp;{item.description}</td>
+              <td style={items.length -1 === i ? {borderBottomLeftRadius: "15px"} : {} }
+                className="itemdesc" >&nbsp;&nbsp;&nbsp;{item.description}</td>
               <td className="itemamount">{item.amount}</td>
-              <td style={items.length -1 === i ? {borderBottomRightRadius: "15px", textAlign: "right"} : {textAlign: "right"} } ><a onClick={() => remove(item.id)} className="delete" href="#">{deletebutton}</a></td>
+              <td style={items.length -1 === i ? {borderBottomRightRadius: "15px", textAlign: "right"} : {textAlign: "right"} } >
+                <a onClick={() => remove(item.id)} className="delete" href="#">{deletebutton}</a>
+              </td>
             </tr>
           ))}
           </tbody>
